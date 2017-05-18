d718e235b80d22781f18b712f31414b9d2b5276e
GreenSunShen <shenyue088@gmail.com>
2017-05-18 12:10:48 -0700

    init_actor change again with data model

--- account1.go
+++ account1.go
@@ -15,8 +15,8 @@ import (
 //	Chaincode - A blank struct for use with Shim (A HyperLedger included go file used for get/put state
 //				and other HyperLedger functions)
 //==============================================================================================================================
-type  SimpleChaincode struct {
-}		
+type SimpleChaincode struct {
+}
 
 //==============================================================================================================================
 //	Account - Defines the structure for an account object. JSON on right tells it what JSON fields to map to
@@ -24,62 +24,65 @@ type  SimpleChaincode struct {
 //==============================================================================================================================
 
 type Actor struct {
-	ActorId   string `json:"actorid"`
-	ActorName string `json:"actorname"`
-	Balance   string  `json:"balance"`
+	ActorId    string `json:"actorid"`
+	ActorName  string `json:"actorname"`
+	Committed  string  `json:"committed"`
+	Reimbursed string `json:"reimbursed"`
+	Awarded    string `json:"awarded"`
+	Spent      string `json:"spent"`
+	Received   string `json:"received"`
+	Delegated  string `json:"delegated"`
 }
-
 
 //award (award id, award name, award status, amount_requested, parent award id(-1))
 type Award struct {
-	AwardId string `json:"awardid"`
-	AwardName string `json:"awardname"`
-	AwardStatus string `json:"awardstatus"`
+	AwardId        string `json:"awardid"`
+	AwardName      string `json:"awardname"`
+	AwardStatus    string `json:"awardstatus"`
 	AwardRequested int `json:"awardrequested"`
-	ParentAwardId string `json:"parentawardid"`
+	ParentAwardId  string `json:"parentawardid"`
 }
 
 //award amount (award_amount_id, award id, award amount, grantor id)
 type AwardAmount struct {
 	AwardAmountId string `json:"awardamountid"`
-	AwardId string `json:"awardid"`
-	AwardAmount int `json:"awardamount"`
-	GrantorId string `json:"grantorid"`
+	AwardId       string `json:"awardid"`
+	AwardAmount   int `json:"awardamount"`
+	GrantorId     string `json:"grantorid"`
 }
 
 //award parties (award party id, award id, role type, account id)
 type AwardParties struct {
 	AwardPartyId string `json:"awardpartyid"`
-	AwardId string `json:"awardid"`
-	RoleType string `json:"roletype"`
-	AccountId string `json:"accountid"`
+	AwardId      string `json:"awardid"`
+	RoleType     string `json:"roletype"`
+	AccountId    string `json:"accountid"`
 }
 
 //reimbursement (reimbursement id, status, award id, amount)
 type Reimbursement struct {
 	ReimbursementId string `json:"reimbursementid"`
-	Status string `json:"status"`
-	AwardId string `json:"awardid"`
-	Amount int `json:"amount"`
-	FromUser string `json:"fromuser"`
-	ToUser string `json:"touser"`
+	Status          string `json:"status"`
+	AwardId         string `json:"awardid"`
+	Amount          int `json:"amount"`
+	FromUser        string `json:"fromuser"`
+	ToUser          string `json:"touser"`
 }
 
 //expenditure (expenditure id, amount, project id, date, type, reimbursement id)
 type Expenditure struct {
-	ExpenditureId string `json:"expenditureid"`
-	Amount int `json:"amount"`
-	ProjectId string `json:"projectid"`
-	Date string `json:"date"`
-	Type string `json:"type"`
-	Status string `json:"status"`
+	ExpenditureId   string `json:"expenditureid"`
+	Amount          int `json:"amount"`
+	ProjectId       string `json:"projectid"`
+	Date            string `json:"date"`
+	Type            string `json:"type"`
+	Status          string `json:"status"`
 	ReimbursementId string `json:"reimbursementid"`
-	FromUser string `json:"fromuser"`
-	ToUser string `json:"touser"`
+	FromUser        string `json:"fromuser"`
+	ToUser          string `json:"touser"`
 }
 
-
-var accountIndexStr = "_accountindex"	  // Define an index variable to track all the accounts stored in the world state
+var accountIndexStr = "_accountindex" // Define an index variable to track all the accounts stored in the world state
 
 // ============================================================================================================================
 //  Main - main - Starts up the chaincode
@@ -91,7 +94,6 @@ func main() {
 	}
 }
 
-
 // ============================================================================================================================
 // SetUp Function - Called after the user deploys the chain code, before demo
 // Function: create 4 actors, update AwardParty struct, update Award struct
@@ -101,7 +103,6 @@ func main() {
 func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	return nil, nil
 }
-
 
 // ============================================================================================================================
 // CommitFund Function - Called when the grantor decided to commit the award
@@ -113,7 +114,6 @@ func (t *SimpleChaincode) CommitFund(stub shim.ChaincodeStubInterface, args []st
 	return nil, nil
 }
 
-
 // ============================================================================================================================
 // ReleaseFund Function - Called when the grantor approves the reimbursement
 // Function: update Expenditure struct (status), update Reimbursement struct (status), update Actor struct (transfer balance),
@@ -123,7 +123,6 @@ func (t *SimpleChaincode) CommitFund(stub shim.ChaincodeStubInterface, args []st
 func (t *SimpleChaincode) ReleaseFund(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	return nil, nil
 }
-
 
 // ============================================================================================================================
 // RequestReimbursement Function - Called when the grantee approves the reimbursement
@@ -135,7 +134,6 @@ func (t *SimpleChaincode) RequestReimbursement(stub shim.ChaincodeStubInterface,
 	return nil, nil
 }
 
-
 // ============================================================================================================================
 // CreateAward Function - Called when the grantee creates an award
 // Function: update Award struct, update AwardParty struct
@@ -144,7 +142,6 @@ func (t *SimpleChaincode) RequestReimbursement(stub shim.ChaincodeStubInterface,
 func (t *SimpleChaincode) CreateAward(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	return nil, nil
 }
-
 
 // ============================================================================================================================
 // RequestAward Function - Called when the grantee request funding of an award
@@ -156,7 +153,6 @@ func (t *SimpleChaincode) RequestAward(stub shim.ChaincodeStubInterface, args []
 	return nil, nil
 }
 
-
 // ============================================================================================================================
 // AllocateSubAward Function - Called when the grantee commits sub-award to sub-grantee
 // Function: update Award struct (create a new sub-award, parent award id needs to be passed), update Account struct (balance transfer),
@@ -167,7 +163,6 @@ func (t *SimpleChaincode) AllocateSubAward(stub shim.ChaincodeStubInterface, arg
 	return nil, nil
 }
 
-
 // ============================================================================================================================
 // Spend Function - Called when the grantee or sub-grantee has an expenditure
 // Function: update Expenditure struct (create a new one), update Account struct (balance transfer),
@@ -177,9 +172,6 @@ func (t *SimpleChaincode) AllocateSubAward(stub shim.ChaincodeStubInterface, arg
 func (t *SimpleChaincode) Spend(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	return nil, nil
 }
-
-
-
 
 // ============================================================================================================================
 // Init Function - Called when the user deploys the chaincode
@@ -200,18 +192,18 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 
 	// Write the state to the ledger, test the network
-	err = stub.PutState("test_key", []byte(strconv.Itoa(Aval)))	
+	err = stub.PutState("test_key", []byte(strconv.Itoa(Aval)))
 	if err != nil {
 		return nil, err
 	}
-	
+
 	var empty []string
-	jsonAsBytes, _ := json.Marshal(empty)								//marshal an emtpy array of strings to clear the account index
+	jsonAsBytes, _ := json.Marshal(empty) //marshal an emtpy array of strings to clear the account index
 	err = stub.PutState(accountIndexStr, jsonAsBytes)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	return nil, nil
 }
 
@@ -222,15 +214,15 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
 	// Handle different functions
-	if function == "init" {										//initialize the chaincode state, used as reset
+	if function == "init" { //initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
-	} else if function == "delete" {									
-		return t.Delete(stub, args)												
-	} else if function == "write" {									
+	} else if function == "delete" {
+		return t.Delete(stub, args)
+	} else if function == "write" {
 		return t.Write(stub, args)
 	} else if function == "init_actor" {
 		return t.init_actor(stub, args)
-	}else if function == "CommitFund"{
+	} else if function == "CommitFund" {
 		return t.CommitFund(stub, args)
 	}
 	//else if function == "transfer_balance" {
@@ -246,10 +238,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 // ============================================================================================================================
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
-	if function == "read" {												
+	if function == "read" {
 		return t.read(stub, args)
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func: " + function) //error
 
 	return nil, errors.New("Received unknown function query " + function)
 }
@@ -266,13 +258,13 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	}
 
 	name = args[0]
-	valAsbytes, err := stub.GetState(name)	
+	valAsbytes, err := stub.GetState(name)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + name + "\"}"
 		return nil, errors.New(jsonResp)
 	}
 
-	return valAsbytes, nil												
+	return valAsbytes, nil
 }
 
 // ============================================================================================================================
@@ -282,9 +274,9 @@ func (t *SimpleChaincode) Delete(stub shim.ChaincodeStubInterface, args []string
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-	
+
 	name := args[0]
-	err := stub.DelState(name)													//remove the key from chaincode state
+	err := stub.DelState(name) //remove the key from chaincode state
 	if err != nil {
 		return nil, errors.New("Failed to delete state")
 	}
@@ -295,16 +287,16 @@ func (t *SimpleChaincode) Delete(stub shim.ChaincodeStubInterface, args []string
 		return nil, errors.New("Failed to get account index")
 	}
 	var accountIndex []string
-	json.Unmarshal(accountsAsBytes, &accountIndex)						
-	
+	json.Unmarshal(accountsAsBytes, &accountIndex)
+
 	//remove account from index
-	for i,val := range accountIndex{
-		if val == name{															//find the correct account
-			accountIndex = append(accountIndex[:i], accountIndex[i+1:]...)			//remove it
+	for i, val := range accountIndex {
+		if val == name { //find the correct account
+			accountIndex = append(accountIndex[:i], accountIndex[i+1:]...) //remove it
 			break
 		}
 	}
-	jsonAsBytes, _ := json.Marshal(accountIndex)									//save the new index
+	jsonAsBytes, _ := json.Marshal(accountIndex) //save the new index
 	err = stub.PutState(accountIndexStr, jsonAsBytes)
 	return nil, nil
 }
@@ -313,16 +305,16 @@ func (t *SimpleChaincode) Delete(stub shim.ChaincodeStubInterface, args []string
 // Write - directly write a variable into chaincode world state
 // ============================================================================================================================
 func (t *SimpleChaincode) Write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var name, value string 
+	var name, value string
 	var err error
 
 	if len(args) != 2 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the variable and value to set")
 	}
 
-	name = args[0]														
+	name = args[0]
 	value = args[1]
-	err = stub.PutState(name, []byte(value))					
+	err = stub.PutState(name, []byte(value))
 	if err != nil {
 		return nil, err
 	}
@@ -335,11 +327,11 @@ func (t *SimpleChaincode) Write(stub shim.ChaincodeStubInterface, args []string)
 func (t *SimpleChaincode) init_actor(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var err error
 
-	//       0        1      2
-	// "accountNo", "bob",  "3500"
+	//       0        1      2..
+	// "accountid", "name",  ...
 
-	if len(args) != 3 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	if len(args) != 8 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 8")
 	}
 
 	//input sanitation
@@ -353,16 +345,49 @@ func (t *SimpleChaincode) init_actor(stub shim.ChaincodeStubInterface, args []st
 	if len(args[2]) <= 0 {
 		return nil, errors.New("3rd argument must be a non-empty string")
 	}
-
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument must be a non-empty string")
+	}
+	if len(args[4]) <= 0 {
+		return nil, errors.New("5th argument must be a non-empty string")
+	}
+	if len(args[5]) <= 0 {
+		return nil, errors.New("6th argument must be a non-empty string")
+	}
+	if len(args[6]) <= 0 {
+		return nil, errors.New("7th argument must be a non-empty string")
+	}
+	if len(args[7]) <= 0 {
+		return nil, errors.New("8th argument must be a non-empty string")
+	}
 
 	actorId := args[0]
 
 	actorName := strings.ToLower(args[1])
 
-	balance, err := strconv.ParseFloat(args[2],64)
-
+	committed, err := strconv.ParseFloat(args[2], 64)
 	if err != nil {
 		return nil, errors.New("3rd argument must be a numeric string")
+	}
+	reimbursed, err := strconv.ParseFloat(args[3], 64)
+	if err != nil {
+		return nil, errors.New("4th argument must be a numeric string")
+	}
+	awarded, err := strconv.ParseFloat(args[4], 64)
+	if err != nil {
+		return nil, errors.New("5th argument must be a numeric string")
+	}
+	spent, err := strconv.ParseFloat(args[5], 64)
+	if err != nil {
+		return nil, errors.New("6th argument must be a numeric string")
+	}
+	received, err := strconv.ParseFloat(args[6], 64)
+	if err != nil {
+		return nil, errors.New("7th argument must be a numeric string")
+	}
+	delegated, err := strconv.ParseFloat(args[7], 64)
+	if err != nil {
+		return nil, errors.New("8th argument must be a numeric string")
 	}
 
 	//check if account already exists
@@ -373,10 +398,15 @@ func (t *SimpleChaincode) init_actor(stub shim.ChaincodeStubInterface, args []st
 
 	res := Actor{}
 	json.Unmarshal(accountAsBytes, &res)
-	if res.ActorId == actorId{
-		return nil, errors.New("This account arleady exists")			
+	if res.ActorId == actorId {
+		return nil, errors.New("This account arleady exists")
 	}
-	balanceStr := strconv.FormatFloat(balance, 'E', -1, 64)
+	committedStr := strconv.FormatFloat(committed, 'f', -1, 64)
+	reimbursedStr := strconv.FormatFloat(reimbursed, 'f', -1, 64)
+	awardedStr := strconv.FormatFloat(awarded, 'f', -1, 64)
+	spentStr := strconv.FormatFloat(spent, 'f', -1, 64)
+	receivedStr := strconv.FormatFloat(received, 'f', -1, 64)
+	delegatedStr := strconv.FormatFloat(delegated, 'f', -1, 64)
 
 	//newActor := Actor{}
 	//newActor.ActorId = actorId
@@ -384,25 +414,25 @@ func (t *SimpleChaincode) init_actor(stub shim.ChaincodeStubInterface, args []st
 	//newActor.Balance = balance
 
 	//build the account json string 
-	str := `{"actorid": "` + actorId + `", "actorName": "` + actorName + `", "balance": "` + balanceStr + `"}`
+	str := `{"actorid": "` + actorId + `", "actorName": "` + actorName + `", "comitted": "` + committedStr + `", "reimbursed": "` + reimbursedStr + `", "awarded": "` + awardedStr + `", "spent": "` + spentStr + `", "received": "` + receivedStr + `", "delegated": "` + delegatedStr + `"}`
 	//jsonAsBytesActor, _ := json.Marshal(newActor)
 	err = stub.PutState(actorId, []byte(str))
 	if err != nil {
 		return nil, err
 	}
-		
+
 	//get the account index
 	accountsAsBytes, err := stub.GetState(accountIndexStr)
 	if err != nil {
 		return nil, errors.New("Failed to get account index")
 	}
 	var accountIndex []string
-	json.Unmarshal(accountsAsBytes, &accountIndex)							
-	
+	json.Unmarshal(accountsAsBytes, &accountIndex)
+
 	//append the index 
 	accountIndex = append(accountIndex, actorId)
 	jsonAsBytes, _ := json.Marshal(accountIndex)
-	err = stub.PutState(accountIndexStr, jsonAsBytes)						
+	err = stub.PutState(accountIndexStr, jsonAsBytes)
 
 	return nil, nil
 }
