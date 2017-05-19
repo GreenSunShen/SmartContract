f66831efe3cd07a6e91231b2783315ebdc6d8ee0
GreenSunShen <shenyue088@gmail.com>
2017-05-18 18:29:15 -0700

    set up testing -- multi users

--- account1.go
+++ account1.go
@@ -49,7 +49,7 @@ type Award struct {
 	AwardId        string `json:"awardid"`
 	AwardName      string `json:"awardname"`
 	AwardStatus    string `json:"awardstatus"`
-	AwardRequested int `json:"awardrequested"`
+	AwardRequested string `json:"awardrequested"`
 	Party AwardParties `json:"awardparties"`
 	Expenses []Expenditure `json:"expenditures"`
 	Reimburses []Reimbursement `json:"reimburses"`
@@ -59,7 +59,7 @@ type Award struct {
 type AwardAmount struct {
 	AwardAmountId string `json:"awardamountid"`
 	AwardId       string `json:"awardid"`
-	AwardAmount   float64 `json:"awardamount"`
+	AwardAmount   string `json:"awardamount"`
 	GrantorId     string `json:"grantorid"`
 }
 
@@ -68,7 +68,7 @@ type AwardAmount struct {
 //reimbursement (reimbursement id, status, award id, amount)
 type Reimbursement struct {
 	ReimbursementId string `json:"reimbursementid"`
-	Amount          int `json:"amount"`
+	Amount          string `json:"amount"`
 	FromActor        string `json:"fromuser"`
 	ToActor          string `json:"touser"`
 	Date            string `json:"date"`
@@ -78,7 +78,7 @@ type Reimbursement struct {
 //expenditure (expenditure id, amount, project id, date, type, reimbursement id)
 type Expenditure struct {
 	ExpenditureId   string `json:"expenditureid"`
-	Amount          float64 `json:"amount"`
+	Amount          string `json:"amount"`
 	Date            string `json:"date"`
 	Type            string `json:"type"`
 	Status          string `json:"status"`
@@ -108,7 +108,7 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 //------------------------------create roles----------------------------------------------
 	// grantor info
 	var act1  = make([]string, 8,8)
-	act1[0] = "101"                //ActorId
+	act1[0] = "ACT-101"                //ActorId
 	act1[1] = "PPM Foundation"     //ActorName
 	act1[2] = "125000"             //Comiitted
 	act1[3] = "55000"              //Reimbursed
@@ -119,7 +119,7 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 
 	// grantee info
 	var act2  = make([]string, 8,8)
-	act2[0] = "102"                     //ActorId
+	act2[0] = "ACT-102"                     //ActorId
 	act2[1] = "Stanford University"      //ActorName
 	act2[2] = ""                        //Comiitted
 	act2[3] = ""                         //Reimbursed
@@ -130,9 +130,9 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 
 	// sub-grantee info
 	var act3  = make([]string, 8,8)
-	act3[0] = "103"                //ActorId
+	act3[0] = "ACT-103"                //ActorId
 	act3[1] = "John Hopkins University"     //ActorName
-	act3[2] = ""               //Comitted
+	act3[2] = ""               //Comiitted
 	act3[3] = ""               //Reimbursed
 	act3[4] = "45000"          //Awarded
 	act3[5] = "12000"          //Spent
@@ -141,9 +141,9 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 
 	// Supplier info -- shows spending form all the grantees and sub-grantees
 	var act4  = make([]string, 8,8)
-	act4[0] = "104"                //ActorId
+	act4[0] = "ACT-104"                //ActorId
 	act4[1] = "Dixon consulting"     //ActorName
-	act4[2] = ""               //Comitted
+	act4[2] = ""               //Comiitted
 	act4[3] = ""               //Reimbursed
 	act4[4] = ""          //Awarded
 	act4[5] = ""          //Spent
@@ -155,6 +155,100 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 	t.init_actor(stub, act2)
 	t.init_actor(stub, act3)
 	t.init_actor(stub, act4)
+
+	//----------------create expenses----------------------------------------------------------
+	// Expense
+	var exp1  = make([]string, 7,7)
+	exp1[0] = "EXP-201"                //ExpenditureId
+	exp1[1] = "3000"     //Amount
+	exp1[2] = "05-02-2017"             //Date
+	exp1[3] = "Travel"              //Type
+	exp1[4] = "Approved"                   //Status
+	exp1[5] = "ACT-102"                   //FromActor --Grantee spending
+	exp1[6] = "ACT-104"                   //ToActor   --Supplier receiving the spending
+
+
+	var exp2  = make([]string, 7,7)
+	exp2[0] = "EXP-202"                //ExpenditureId
+	exp2[1] = "8000"     //Amount
+	exp2[2] = "05-03-2017"             //Date
+	exp2[3] = "Equipment"              //Type
+	exp2[4] = "Pending"                   //Status
+	exp2[5] = "ACT-102"                   //FromActor --Grantee spending
+	exp2[6] = "ACT-104"                   //ToActor   --Supplier receiving the spending
+
+	var exp3 = make([]string, 7,7)
+	exp3[0] = "EXP-203"                //ExpenditureId
+	exp3[1] = "4000"     //Amount
+	exp3[2] = "05-04-2017"             //Date
+	exp3[3] = "Training"              //Type
+	exp3[4] = "Approved"                   //Status
+	exp3[5] = "ACT-102"                   //FromActor --Grantee spending
+	exp3[6] = "ACT-104"
+
+	var exp4 = make([]string, 7,7)
+	exp4[0] = "EXP-204"                //ExpenditureId
+	exp4[1] = "3000"     //Amount
+	exp4[2] = "05-09-2017"             //Date
+	exp4[3] = "Software License"              //Type
+	exp4[4] = "Approved"                   //Status
+	exp4[5] = "ACT-102"                   //FromActor --Grantee spending
+	exp4[6] = "ACT-104"
+
+	var exp5 = make([]string, 7,7)
+	exp5[0] = "EXP-205"                //ExpenditureId
+	exp5[1] = "5000"     //Amount
+	exp5[2] = "05-11-2017"             //Date
+	exp5[3] = "Specimens"              //Type
+	exp5[4] = "Approved"                   //Status
+	exp5[5] = "ACT-102"                   //FromActor --Grantee spending
+	exp5[6] = "ACT-104"
+
+	var exp6 = make([]string, 7,7)
+	exp6[0] = "EXP-206"                //ExpenditureId
+	exp6[1] = "2000"     //Amount
+	exp6[2] = "05-12-2017"             //Date
+	exp6[3] = "Consultancy"              //Type
+	exp6[4] = "Approved"                   //Status
+	exp6[5] = "ACT-103"                   //FromActor --Grantee spending
+	exp6[6] = "ACT-104"
+
+	var exp7 = make([]string, 7,7)
+	exp7[0] = "EXP-207"                //ExpenditureId
+	exp7[1] = "7500"     //Amount
+	exp7[2] = "05-15-2017"             //Date
+	exp7[3] = "Equipment"              //Type
+	exp7[4] = "Pending"                   //Status
+	exp7[5] = "ACT-103"                   //FromActor --Grantee spending
+	exp7[6] = "ACT-104"
+
+	var exp8 = make([]string, 7,7)
+	exp8[0] = "EXP-208"                //ExpenditureId
+	exp8[1] = "1000"     //Amount
+	exp8[2] = "05-16-2017"             //Date
+	exp8[3] = "Travel"              //Type
+	exp8[4] = "Approved"                   //Status
+	exp8[5] = "ACT-103"                   //FromActor --Grantee spending
+	exp8[6] = "ACT-104"
+
+	var exp9 = make([]string, 7,7)
+	exp9[0] = "EXP-209"                //ExpenditureId
+	exp9[1] = "1500"     //Amount
+	exp9[2] = "05-19-2017"             //Date
+	exp9[3] = "Training"              //Type
+	exp9[4] = "Approved"                   //Status
+	exp9[5] = "ACT-103"                   //FromActor --Grantee spending
+	exp9[6] = "ACT-104"
+
+	//t.init_expenditure(stub, exp1)
+	//t.init_expenditure(stub, exp2)
+	//t.init_expenditure(stub, exp3)
+	//t.init_expenditure(stub, exp4)
+	//t.init_expenditure(stub, exp5)
+	//t.init_expenditure(stub, exp6)
+	//t.init_expenditure(stub, exp7)
+	//t.init_expenditure(stub, exp8)
+	//t.init_expenditure(stub, exp9)
 ////----------------------------create award -------------------------------------------------
 //	var award1 []string{}
 //
@@ -163,6 +257,108 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 
 	return nil, nil
 }
+
+
+func (t *SimpleChaincode) init_expenditure(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var err error
+
+	//       0        1      2..
+	// "accountid", "name",  ...
+
+	if len(args) != 7 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 7")
+	}
+
+	//input sanitation
+	fmt.Println("- start init acount")
+	if len(args[0]) <= 0 {
+		return nil, errors.New("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return nil, errors.New("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return nil, errors.New("3rd argument must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return nil, errors.New("4th argument must be a non-empty string")
+	}
+	if len(args[4]) <= 0 {
+		return nil, errors.New("5th argument must be a non-empty string")
+	}
+	if len(args[5]) <= 0 {
+		return nil, errors.New("6th argument must be a non-empty string")
+	}
+	if len(args[6]) <= 0 {
+		return nil, errors.New("7th argument must be a non-empty string")
+	}
+
+	/*
+	     exp9[0] = "EXP-209"                //ExpenditureId
+		exp9[1] = "1500"     //Amount
+		exp9[2] = "05-19-2017"             //Date
+		exp9[3] = "Training"              //Type
+		exp9[4] = "Approved"                   //Status
+	    exp9[5] = "ACT-103"                   //FromActor --Grantee spending
+	    exp9[6] = "ACT-104"
+	*/
+
+	expId := args[0]
+
+	expAmount, err := strconv.ParseFloat(args[1], 64)   //strings.ToLower(args[1])
+
+	if err != nil {
+		return nil, errors.New("2nd argument must be a numeric string")
+	}
+
+	expDate := args[2]
+	expType := args[3]
+	expStatus := args[4]
+	fromActor := args[5]
+	toActor := args[6]
+
+
+
+	//check if account already exists
+	accountAsBytes, err := stub.GetState(expId)
+	if err != nil {
+		return nil, errors.New("Failed to get expenditure id")
+	}
+
+	exp := Expenditure{}
+	json.Unmarshal(accountAsBytes, &exp)
+	if exp.ExpenditureId == expId {
+		return nil, errors.New("This expenditure arleady exists")
+	}
+
+
+      expAmountStr := strconv.FormatFloat(expAmount, 'f', -1, 64)
+
+	//newActor := Actor{}
+	//newActor.ActorId = actorId
+	//newActor.ActorName = actorName
+	//newActor.Balance = balance
+
+	//	   expDate := arg[2]
+	///  expType := arg[3]
+	/// expStatus := arg[4]
+	//fromActor := arg[5]
+	//toActor := arg[6]
+
+
+	//build the expenditure json string
+	str := `{"expenditureid": "` + expId + `", "amount": "` + expAmountStr + `", "date": "` + expDate + `", "type": "` + expType + `", "status": "` + expStatus + `", "fromactor": "` + fromActor + `", "toactor": "` + toActor + `"}`
+	//jsonAsBytesActor, _ := json.Marshal(newActor)
+	err = stub.PutState(expId, []byte(str))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+
+
 
 
 // ============================================================================================================================
@@ -174,8 +370,9 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 func (t *SimpleChaincode) ReleaseFund(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	//arg[0] actor id
 	//arg[1] award id
-	//arg[3] ... exp id
-
+	//arg[2] ... exp id
+	//var expenseIds []string
+	//for i = 2;
 
 
 	return nil, nil
@@ -272,8 +469,8 @@ func (t *SimpleChaincode) Spend(stub shim.ChaincodeStubInterface, args []string)
 
 	// populate expenditure
 	exp := Expenditure{}
-	exp.Amount = amount
-	exp.Date = string(current_time.String())
+	exp.Amount = strconv.FormatFloat(amount, 'f', -1, 64)
+	exp.Date = current_time.String()
 	exp.Type = args[3]
 	exp.ExpenditureId = "ex1"
 	exp.FromActor = resA.ActorId
@@ -359,9 +556,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Spend(stub, args)
 	} else if function == "releasefund"{
 		return t.ReleaseFund(stub, args)
-	} else if function == "transferbalance" {
-		return t.transfer_balance(stub, args)
 	}
+	//else if function == "transferbalance" {
+	//	return t.transfer_balance(stub, args)
+	//}
 
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
@@ -476,24 +674,6 @@ func (t *SimpleChaincode) init_actor(stub shim.ChaincodeStubInterface, args []st
 	if len(args[1]) <= 0 {
 		return nil, errors.New("2nd argument must be a non-empty string")
 	}
-	if len(args[2]) <= 0 {
-		return nil, errors.New("3rd argument must be a non-empty string")
-	}
-	if len(args[3]) <= 0 {
-		return nil, errors.New("4th argument must be a non-empty string")
-	}
-	if len(args[4]) <= 0 {
-		return nil, errors.New("5th argument must be a non-empty string")
-	}
-	if len(args[5]) <= 0 {
-		return nil, errors.New("6th argument must be a non-empty string")
-	}
-	if len(args[6]) <= 0 {
-		return nil, errors.New("7th argument must be a non-empty string")
-	}
-	if len(args[7]) <= 0 {
-		return nil, errors.New("8th argument must be a non-empty string")
-	}
 
 	actorId := args[0]
 
@@ -607,6 +787,7 @@ func (t *SimpleChaincode) transfer_balance(stub shim.ChaincodeStubInterface, arg
 
 	switch args[3] {
 		case "spend" :
+			fmt.Println("INSIDE CASE SPEND==========================")
 			AwardA, err := strconv.ParseFloat(resA.Awarded, 64)
 			if err != nil {
 				return nil, err
