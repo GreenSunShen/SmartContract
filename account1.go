42ff0e39d9de3f86edd602b983e4de7ddb59e41e
GreenSunShen <shenyue088@gmail.com>
2017-05-18 18:37:30 -0700

    set up testing -- multi users-2

--- account1.go
+++ account1.go
@@ -112,17 +112,17 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 	act1[1] = "PPM Foundation"     //ActorName
 	act1[2] = "125000"             //Comiitted
 	act1[3] = "55000"              //Reimbursed
-	act1[4] = ""                   //Awarded
-	act1[5] = ""                   //Spent
-	act1[6] = ""                   //Received
-	act1[7] = ""                   //Delegated
+	act1[4] = "-1"                   //Awarded
+	act1[5] = "-1"                   //Spent
+	act1[6] = "-1"                   //Received
+	act1[7] = "-1"                   //Delegated
 
 	// grantee info
 	var act2  = make([]string, 8,8)
 	act2[0] = "ACT-102"                     //ActorId
 	act2[1] = "Stanford University"      //ActorName
-	act2[2] = ""                        //Comiitted
-	act2[3] = ""                         //Reimbursed
+	act2[2] = "-1"                        //Comiitted
+	act2[3] = "-1"                         //Reimbursed
 	act2[4] = "125000"                 //Awarded
 	act2[5] = "23000"                  //Spent
 	act2[6] = "55000"                 //Received
@@ -132,23 +132,23 @@ func (t *SimpleChaincode) SetUp(stub shim.ChaincodeStubInterface, args []string)
 	var act3  = make([]string, 8,8)
 	act3[0] = "ACT-103"                //ActorId
 	act3[1] = "John Hopkins University"     //ActorName
-	act3[2] = ""               //Comiitted
-	act3[3] = ""               //Reimbursed
+	act3[2] = "-1"               //Comiitted
+	act3[3] = "-1"               //Reimbursed
 	act3[4] = "45000"          //Awarded
 	act3[5] = "12000"          //Spent
 	act3[6] = "25000"          //Received
-	act3[7] = ""               //Delegated
+	act3[7] = "-1"               //Delegated
 
 	// Supplier info -- shows spending form all the grantees and sub-grantees
 	var act4  = make([]string, 8,8)
 	act4[0] = "ACT-104"                //ActorId
 	act4[1] = "Dixon consulting"     //ActorName
-	act4[2] = ""               //Comiitted
-	act4[3] = ""               //Reimbursed
-	act4[4] = ""          //Awarded
-	act4[5] = ""          //Spent
+	act4[2] = "-1"               //Comiitted
+	act4[3] = "-1"               //Reimbursed
+	act4[4] = "-1"          //Awarded
+	act4[5] = "-1"          //Spent
 	act4[6] = "35000"          //Received
-	act4[7] = ""               //Delegated
+	act4[7] = "-1"               //Delegated
 
 
 	t.init_actor(stub, act1)
